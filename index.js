2424364247a0a2b44e007f5088a753d5d6fede91
Anurag Bansal <devanuragbansal@gmail.com>
2025-09-01 22:48:44 +0530

    Add database connection setup and update server initialization

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import app from "./src/app.js";
 import config from "./src/config/config.js";
+import connectDB from "./src/config/connectDB.js";
 
 const PORT = config.port;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
+
+  await connectDB();
 });
--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 const config = {
   port: process.env.PORT || 3000,
+  mongoURI: process.env.MONGO_URI || "mongodb://localhost:27017/BitChat",
 };
 
 export default config;
